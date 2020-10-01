6b0107248788c70ee16af21078bf88d03de04c95
Denys <d.o.sheremet@student.tudelft.nl>
2020-10-01 23:28:56 +0200

    add word count

--- main.js
+++ main.js
@@ -2,10 +2,28 @@ var myVar;
 var previousText = "";
 var hardcoreMode = false;
 var div;
+var currentArea;
+var previousArea;
+var allTextArea;
+var inputField;
+
 
 window.addEventListener('load', function() {
     div = document.getElementById('current-text');
+    currentArea = document.getElementById("current-text");
+    previousArea = document.getElementById("previous-text");
+    input = document.getElementById('input-field');
+    allTextArea = document.getElementById("all-text");
+    inputField = document.getElementById("input-field");
+    input.onkeydown = function() {
+        var key = event.keyCode || event.charCode;
+        if (key == 8) {
+            clearCurrent();
+        }
+    };
 });
+
+
 
 var dictionary = 'https://rawcdn.githack.com/maheshmurag/bjspell/master/dictionary.js/en_US.js';
 var lang = BJSpell(dictionary, function() {});
@@ -14,7 +32,7 @@ var lang = BJSpell(dictionary, function() {});
 function spellCheck() {
     console.log("checking spelling...");
 
-    document.getElementById("score").innerHTML = calculateScore();
+    calculateScore();
 }
 
 function calculateScore(final = false) {
@@ -33,18 +51,16 @@ function calculateScore(final = false) {
         if (!correct) { mistakes++; }
         console.log("Words " + wordList.length + "Mistakes: " + mistakes);
     })
-    return wordList.length - (mistakes * 3);
+    document.getElementById("wordCount").innerHTML = wordList.length;
+    document.getElementById("score").innerHTML = wordList.length - (mistakes * 3);
 }
 
 
 function updateText() {
     // updates the text that is visible in the divs below the input field
 
-    var currentArea = document.getElementById("current-text");
-    var previousArea = document.getElementById("previous-text");
-    var allTextArea = document.getElementById("all-text");
 
-    currentArea.innerHTML = document.getElementById("input-field").value;
+    currentArea.innerHTML = inputField.value;
 
     // scroll down
     allTextArea.scrollTop = allTextArea.scrollHeight;;
@@ -54,33 +70,30 @@ function updateText() {
 
     // stop previous timer and start a new one
     clearTimeout(myVar);
-    myVar = setTimeout(function() { clearCurrent(currentArea, previousArea); }, 1000); // game over if there is no new input in X milliseconds
+    myVar = setTimeout(function() { clearCurrent(); }, 1000); // game over if there is no new input in X milliseconds
 
 }
 
-function clearCurrent(currentArea, previousArea) {
+function clearCurrent() {
     // sets the score to zero and moves the text you've written into the backlog
 
     alert("Your score was: " + calculateScore(final = true));
-
-    // put text from current into previous
-    var inputField = document.getElementById("input-field");
 
     previousText += inputField.value;
     previousText += "\r____\r\r";
     previousArea.innerHTML = previousText;
 
     // remove text from current field
-    document.getElementById("input-field").value = "";
+    inputField.value = "";
     currentArea.innerHTML = "";
 }
 
 function clearText() {
     // this method removes all of the text that was written before.
     previousText = "";
-    document.getElementById("input-field").value = "";
-    document.getElementById("current-text").innerHTML = "";
-    document.getElementById("previous-text").innerHTML = "";
+    inputField.value = "";
+    currentArea.innerHTML = "";
+    previousArea.innerHTML = "";
 }
 
 function toggleHardcoreMode() {
